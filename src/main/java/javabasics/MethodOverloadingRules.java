92d9865a84685d15b29d5b48b4f0bf846db256e1
sumit <sumit.chawla.india@gmail.com>
2020-04-14 12:01:56 +0530

    method overloading rules class added

---
+++ src/main/java/javabasics/MethodOverloadingRules.java
@@ -0,0 +1,34 @@
+package javabasics;
+
+class First {
+	// we cannot overload methods in java based on different access modifiers
+	//method is already defined error is reported by compiler
+//	private void method(){
+//		System.out.println("method first");
+//	}
+//
+//	public void method(){
+//		System.out.println("method first");
+//	}
+
+//	Return type of method is not part of method signature,
+// so just changing the return type will not overload a method in Java.
+// In fact, just changing the return type will result in compile time error as "duplicate method X in type Y
+
+	// in java method overloading can not be done based on return type
+//	private void method(){
+//		System.out.println("method first");
+//	}
+//
+//	private int method(){
+//		System.out.println("method first");
+//	}
+
+//	First and foremost rule to overload a method in Java is to change method signature.
+// method signature is made of number of arguments,
+// type of arguments
+// and order of arguments if they are of different types.
+
+}
+public class MethodOverloadingRules {
+}
